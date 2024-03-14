219cbb0b7bde5dd220a3580ab982f59d468a2ec8
sharp378 <107257160+sharp378@users.noreply.github.com>
2024-03-14 08:32:35 -0400

    Add files (#1)
    * add files
    
    * fix plugin build, add compose yaml

---
+++ cloudwatch-plugin/CloudWatch.java
@@ -0,0 +1,122 @@
+package github.metalshark.cloudwatch;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import github.metalshark.cloudwatch.listeners.*;
+import github.metalshark.cloudwatch.runnables.JavaStatisticsRunnable;
+import github.metalshark.cloudwatch.runnables.MinecraftStatisticsRunnable;
+import github.metalshark.cloudwatch.runnables.TickRunnable;
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.regions.internal.util.EC2MetadataUtils;
+import software.amazon.awssdk.services.cloudwatch.model.Dimension;
+
+import java.util.Map;
+import java.util.concurrent.*;
+
+public class CloudWatch extends JavaPlugin {
+
+  /*@Getter
+    private ChunkLoadListener chunkLoadListener = new ChunkLoadListener();*/
+
+    @Getter
+    private PlayerJoinListener playerJoinListener = new PlayerJoinListener();
+
+    @Getter
+    private TickRunnable tickRunnable = new TickRunnable();
+
+    @Getter
+    private final static Map<String, EventCountListener> eventCountListeners = new ConcurrentHashMap<>();
+
+  /*private final static ThreadFactory javaStatisticsThreadFactory = new ThreadFactoryBuilder()
+        .setNameFormat("CloudWatch - Java Statistics")
+        .build();*/
+    private final static ThreadFactory minecraftStatisticsThreadFactory = new ThreadFactoryBuilder()
+        .setNameFormat("CloudWatch - Minecraft Statistics")
+        .build();
+
+  //private ScheduledExecutorService javaStatisticsExecutor;
+    private ScheduledExecutorService minecraftStatisticsExecutor;
+
+    @Getter
+    private static Dimension dimension;
+
+    @Override
+    public void onEnable() {
+        try {
+            dimension = Dimension
+                .builder()
+                .name("Per-Instance Metrics")
+                .value(EC2MetadataUtils.getInstanceId())
+                .build();
+        } catch (SdkClientException exception) {
+            getLogger().warning("The CloudWatch plugin only works on EC2 instances.");
+            this.setEnabled(false);
+            return;
+        }
+
+        final PluginManager pluginManager = Bukkit.getPluginManager();
+
+      //pluginManager.registerEvents(chunkLoadListener.init(), this);
+        pluginManager.registerEvents(playerJoinListener.init(), this);
+
+        /*eventCountListeners.put("ChunksPopulated", new ChunkPopulateListener());
+        eventCountListeners.put("CreaturesSpawned", new CreatureSpawnListener());
+        eventCountListeners.put("EntityDeaths", new EntityDeathListener());
+        eventCountListeners.put("InventoriesClosed", new InventoryCloseListener());
+        eventCountListeners.put("InventoriesOpened", new InventoryOpenListener());
+        eventCountListeners.put("InventoryClicks", new InventoryClickListener());
+        eventCountListeners.put("InventoryDrags", new InventoryDragListener());
+        eventCountListeners.put("ItemsDespawned", new ItemSpawnListener());
+        eventCountListeners.put("ItemsSpawned", new ItemDespawnListener());
+        eventCountListeners.put("PlayerDropItems", new PlayerDropItemListener());
+        eventCountListeners.put("PlayerExperienceChanges", new PlayerExpChangeListener());
+        eventCountListeners.put("PlayerInteractions", new PlayerInteractListener());
+        eventCountListeners.put("ProjectilesLaunched", new ProjectileLaunchListener());
+        eventCountListeners.put("StructuresGrown", new StructureGrowListener());
+        eventCountListeners.put("TradesSelected", new TradeSelectListener());
+
+        for (Map.Entry<String, EventCountListener> entry : eventCountListeners.entrySet()) {
+            final EventCountListener listener = entry.getValue();
+            pluginManager.registerEvents(listener, this);
+        }
+
+        javaStatisticsExecutor = Executors.newSingleThreadScheduledExecutor(javaStatisticsThreadFactory);
+        javaStatisticsExecutor.scheduleAtFixedRate(new JavaStatisticsRunnable(), 0, 1, TimeUnit.MINUTES);*/
+
+        minecraftStatisticsExecutor = Executors.newSingleThreadScheduledExecutor(minecraftStatisticsThreadFactory);
+        minecraftStatisticsExecutor.scheduleAtFixedRate(new MinecraftStatisticsRunnable(), 0, 1, TimeUnit.MINUTES);
+
+        Bukkit.getServer().getScheduler().runTaskTimerAsynchronously(this, tickRunnable, 1, 1);
+    }
+
+    @Override
+    public void onDisable() {
+       /*ChunkLoadEvent.getHandlerList().unregister(chunkLoadListener);
+        ChunkUnloadEvent.getHandlerList().unregister(chunkLoadListener);*/
+        PlayerJoinEvent.getHandlerList().unregister(playerJoinListener);
+        PlayerQuitEvent.getHandlerList().unregister(playerJoinListener);
+
+       /*for (Map.Entry<String, EventCountListener> entry : eventCountListeners.entrySet()) {
+            final Listener listener = entry.getValue();
+            HandlerList.unregisterAll(listener);
+        }
+
+        if (javaStatisticsExecutor != null) javaStatisticsExecutor.shutdown();,*/
+        if (minecraftStatisticsExecutor != null) minecraftStatisticsExecutor.shutdown();
+    }
+
+    public static CloudWatch getPlugin() {
+        return getPlugin(CloudWatch.class);
+    }
+
+}
+
---
+++ cloudwatch-plugin/runnables/MinecraftStatisticsRunnable.java
@@ -0,0 +1,95 @@
+package github.metalshark.cloudwatch.runnables;
+
+import github.metalshark.cloudwatch.CloudWatch;
+import github.metalshark.cloudwatch.listeners.*;
+import software.amazon.awssdk.services.cloudwatch.CloudWatchClient;
+import software.amazon.awssdk.services.cloudwatch.model.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MinecraftStatisticsRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        final CloudWatch plugin = CloudWatch.getPlugin();
+        final Dimension dimension = CloudWatch.getDimension();
+
+      /*final ChunkLoadListener chunkLoadListener = plugin.getChunkLoadListener();
+        final double chunksLoaded = chunkLoadListener.getMaxAndReset();*/
+
+        final PlayerJoinListener playerJoinListener = plugin.getPlayerJoinListener();
+        final double onlinePlayers = playerJoinListener.getMaxAndReset();
+
+      /*final TickRunnable tickRunnable = plugin.getTickRunnable();
+        final double maxTickTime = tickRunnable.getMaxElapsedMillisAndReset();
+        final double ticksPerMinute = tickRunnable.getNumberOfTicksAndReset();
+        final double ticksPerSecond = ticksPerMinute / 60;*/
+
+        try (final CloudWatchClient cw = CloudWatchClient.builder().build()) {
+
+          /*final MetricDatum chunksLoadedMetric = MetricDatum
+                .builder()
+                .metricName("ChunksLoaded")
+                .unit(StandardUnit.COUNT)
+                .value(chunksLoaded)
+                .dimensions(dimension)
+                .build();*/
+            final MetricDatum onlinePlayersMetric = MetricDatum
+                .builder()
+                .metricName("OnlinePlayers")
+                .unit(StandardUnit.COUNT)
+                .value(onlinePlayers)
+                .dimensions(dimension)
+                .build();
+          /*final MetricDatum maxTickTimeMetric = MetricDatum
+                .builder()
+                .metricName("MaxTickTime")
+                .unit(StandardUnit.MILLISECONDS)
+                .value(maxTickTime)
+                .dimensions(dimension)
+                .build();
+            final MetricDatum ticksPerSecondMetric = MetricDatum
+                .builder()
+                .metricName("TicksPerSecond")
+                .unit(StandardUnit.COUNT)
+                .value(ticksPerSecond)
+                .dimensions(dimension)
+                .build();*/
+
+            final Set<MetricDatum> metricDatumSet = new HashSet<>();
+          /*metricDatumSet.add(chunksLoadedMetric);
+            metricDatumSet.add(maxTickTimeMetric);*/
+            metricDatumSet.add(onlinePlayersMetric);
+          //metricDatumSet.add(ticksPerSecondMetric);
+
+            CloudWatch
+                .getEventCountListeners()
+                .forEach((name, listener) -> {
+                    final double count = listener.getCountAndReset();
+                    metricDatumSet.add(MetricDatum
+                        .builder()
+                        .metricName(name)
+                        .unit(StandardUnit.COUNT)
+                        .value(count)
+                        .dimensions(dimension)
+                        .build());
+                });
+
+            MetricDatum[] metrics = new MetricDatum[metricDatumSet.size()];
+            metricDatumSet.toArray(metrics);
+
+            final PutMetricDataRequest request = PutMetricDataRequest
+                .builder()
+                .namespace("Minecraft")
+                .metricData(metrics)
+                .build();
+
+            final PutMetricDataResponse result = cw.putMetricData(request);
+        } catch (CloudWatchException e) {
+            CloudWatch.getPlugin().getLogger().severe(e.awsErrorDetails().errorMessage());
+        }
+    }
+
+}
+
